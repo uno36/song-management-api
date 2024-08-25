b903754469caee148e1ae495260c924b851afdf8
Emmanuel Tofa <emmanuelutofa@gmail.com>
2024-08-25 11:18:35 +0200

    create song model

---
+++ models/song.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const songSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  album: { type: String, required: true },
+  genre: { type: String, required: true },
+});
+
+const Song = mongoose.model('Song', songSchema);
+
+export default Song;
